ebda6239bb2211bcea211810d49160f073a88fde
great_hayat <greathayat@gmail.com>
2023-03-19 02:03:47 +0500

    implemented user crud

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -9,5 +9,56 @@ export const createUser = async (req: Request, res: Response) => {
   const user = await userReporitory.create({ username, email, gender });
   const response = await userReporitory.save(user);
 
-  res.status(200).send({ success: true, user: response });
+  res.status(200).json({ success: true, user: response });
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+  const {
+    params: { id },
+    body: userPayload,
+  } = req;
+
+  const userId: number = parseInt(id);
+  const user = await userReporitory.findOneBy({ id: userId });
+
+  if (!user) {
+    return res.status(404).json({ error: true, message: "User not found" });
+  }
+
+  const response = await userReporitory.merge(user, userPayload);
+  res.status(200).json({ success: true, user: response });
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  const {
+    params: { id },
+  } = req;
+
+  const userId: number = parseInt(id);
+  const user = await userReporitory.delete({ id: userId });
+
+  if (user.affected === 0) {
+    return res.status(404).json({ error: true, message: "User not found" });
+  }
+
+  res.status(200).json({ message: "User deleted successfully" });
+};
+
+export const getAllUsers = async (req: Request, res: Response) => {
+  const users = await userReporitory.find();
+  res.status(200).json({ success: true, users });
+};
+
+export const getUserById = async (req: Request, res: Response) => {
+  const {
+    params: { id },
+  } = req;
+
+  const userId: number = parseInt(id);
+  const user = await userReporitory.findOneBy({ id: userId });
+
+  if (!user) {
+    return res.status(404).json({ error: true, message: "User not found" });
+  }
+  res.status(200).json({ success: true, user });
 };
--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,9 +1,19 @@
 import { Router } from "express";
-import { createUser } from "../../controllers/user";
+import {
+  createUser,
+  deleteUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+} from "../../controllers/user";
 import { asyncHandler } from "../../middleware/asyncHandler";
 
 const router: Router = Router();
 
+router.get("/", asyncHandler(getAllUsers));
 router.post("/", asyncHandler(createUser));
+router.get("/:id", asyncHandler(getUserById));
+router.put("/:id", asyncHandler(updateUser));
+router.delete("/:id", asyncHandler(deleteUser));
 
 export default router;
